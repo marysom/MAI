9c13f3bf6c738c1ccc4eab71ac6a8586e8403fed
marysom <37898897+marysom@users.noreply.github.com>
2019-09-23 01:15:57 +0300

    Delete 2.sql

--- data_management/hw/2.sql
+++
@@ -1,6 +0,0 @@
---1.1 SELECT , LIMIT - выбрать 10 записей из таблицы ratings (Для всех дальнейших запросов выбирать по 10 записей, если не указано иное)
-SELECT *
-FROM ratings
-ORDER BY rating
-LIMIT 10
-;
